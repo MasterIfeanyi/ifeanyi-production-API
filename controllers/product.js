6381c59ad76a26e338c0f8ea1dddcc7c4ee868c6
IFEANYI <chimaifeanyi29@gmail.com>
2024-03-12 10:17:39 +0100

    first commit

---
+++ controllers/product.js
@@ -0,0 +1,101 @@
+const Products = require("../model/product"); // ->this line
+const redis = require("redis");
+const {client} = require("../index.mjs")
+
+const getProducts = async (req, res) => {
+    const results = await Products.find({});
+    if (result?.length === 0 || !result) {
+        return res.sendStatus(404)
+    }
+    res.json(results);
+}
+
+
+
+const getProductByName = async(req, res) => {
+    const {name} = req.params;
+
+    let result;
+
+    
+    try {
+        
+        // use the key to get data from the cache
+        const cachedData = await redis.get("cachedData");
+
+        // If data exists in the cache, return it
+        if(cachedData) {
+            result = JSON.parse(cachedData);
+        }  else {
+            result = await Products.findOne({name});
+            console.log(result)
+            if (result?.length === 0 || !result) {
+                return res.status(200).send("API returned an empty array");
+            }
+            await client.set(species, JSON.stringify(result));
+        }
+
+        res.json(result);
+
+    } catch (error) {
+        console.error(error);
+        res.status(404).send("Data unavailable");
+    }
+}
+
+
+const createProduct = async (req, res) => {
+    const {name, price, category} = req.body;
+    const products = await Products.create({name, price, category});
+    if (!products) return res.status(404).json({ "message": "unable to create" });
+    res.json(products);
+}
+
+
+
+const updateAProduct = async (req, res) => {
+    const {name} = req.params;
+
+    const {name: newName, price, category} = req.body;
+
+    const product = await Products.findOne({name});    
+
+
+
+    if (product?.length === 0 || !product) {
+        return res.status(204).json({ "message": `No user matches an ID ${id}.`});
+    }
+
+    
+    try {
+
+        product.name = newName;
+        product.price = price;
+        product.category = category; 
+        
+        const result = await product.save();
+
+        return res.status(200).json(result);
+
+    } catch (error) {
+        console.error(error)
+    }
+    
+}
+
+
+const deleteAllProducts = async(req, res) => {
+    const results = await Products.deleteMany({});
+    if (!results) return res.status(200).json({ "message": "No results found" });
+    res.json(results);
+}
+
+const deleteProductByName = async(req, res) => {
+    const {name} = req.params;
+    const results = await Products.findOneAndDelete({name});
+    if (!results) return res.status(200).json({ "message": "No results found" });
+    res.status(204).json({"message": "deleted"});
+}
+
+
+module.exports = {getProducts, getProductByName, deleteProductByName, createProduct, deleteAllProducts, updateAProduct}; 
---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+// swagger ui
+const swaggerjsdoc = require("swagger-jsdoc");
+const swaggerui = require("swagger-ui-express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const connectDB = require('./config/dbConn.js');
+require("dotenv").config
+const bodyParser = require("body-parser");
+const corsOptions = require("./config/corsOptions.js")
+
+const swaggerSpecs = require("./docs/swagger.js")
+
+const {createClient} = require("redis");
+
+
+// redis port
+const REDIS_PORT = 6379;
+
+// create redis client
+export const client = createClient();
+
+
+
+(async () => { 
+    
+    await client.on('error', err => console.log('Error ' + err));
+
+    await client.connect(); 
+
+})();
+
+
+
+const app = express();
+
+app.use(express.json())
+
+
+// Connect to MongoDB
+connectDB(); 
+
+
+// port that the server is listening on.
+const port = 3500;
+
+
+// middleware to handle url encoded data
+// app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(bodyParser.json());
+
+app.use(cors(corsOptions));
+
+
+app.use("/home", require("./routes/route.js"))
+
+
+// the route for api documentation.
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(swaggerSpecs))
+
+
+// our server is listening on port 3000
+mongoose.connection.once("open", () => {
+    console.log("Connected to MongoDB")
+    app.listen(port, () => console.log(`Server running on Port ${port}`))
+})
